aacc84572f6298b9c4a9101480e3ee4c9cd65b8d
Chris Horn <chris.horn@metacortex.com>
2022-05-04 08:24:57 +0100

    saved

---
+++ contracts/ASSET0.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract ASSET0 is ERC1155, Ownable {
+    uint256 public constant ASSET0 = 0;
+
+    constructor()
+        public
+        ERC1155(
+            "https://bafybeiflvp7bbbsptj7tnya3b2ta5d5l7mm5on3yl74nzxxd4erv7ff5fu.ipfs.nftstorage.link/metadata/${id}"
+        )
+    {
+        _mint(msg.sender, ASSET0, 50000, "");
+    }
+
+    function mint(address account, uint256 id, uint256 amount) public onlyOwner {
+        _mint(account, id, amount, "");
+    }
+    /*
+     * Override isApprovedForAll to auto-approve OS's proxy contract
+     */
+    function isApprovedForAll(address _owner, address _operator)
+        public
+        view
+        override
+        returns (bool isOperator)
+    {
+        // if OpenSea's ERC1155 Proxy Address is detected, auto-return true
+        if (_operator == address(0x207Fa8Df3a17D96Ca7EA4f2893fcdCb78a304101)) {
+            return true;
+        }
+        // otherwise, use the default ERC1155.isApprovedForAll()
+        return ERC1155.isApprovedForAll(_owner, _operator);
+    }
+}
--- scripts/mint.js
+++ scripts/mint.js
@@ -2,31 +2,31 @@ const { task } = require("hardhat/config");
 const { getContract } = require("../helpers");
 const fetch = require("node-fetch");
 
-task("mint", "Mints from the NFT contract")
+task("mint", "Mints from the ASSET0 contract")
 .addParam("address", "The address to receive a token")
 .setAction(async function (taskArguments, hre) {
-    const contract = await getContract("NFT", hre);
+    const contract = await getContract("ASSET0", hre);
     const transactionResponse = await contract.mintTo(taskArguments.address, {
         gasLimit: 500_000,
     });
     console.log(`Transaction Hash: ${transactionResponse.hash}`);
 });
 
-task("set-base-token-uri", "Sets the base token URI for the deployed smart contract")
-.addParam("baseUrl", "The base of the tokenURI endpoint to set")
-.setAction(async function (taskArguments, hre) {
-    const contract = await getContract("NFT", hre);
-    const transactionResponse = await contract.setBaseTokenURI(taskArguments.baseUrl, {
-        gasLimit: 500_000,
-    });
-    console.log(`Transaction Hash: ${transactionResponse.hash}`);
-});
+// task("set-base-token-uri", "Sets the base token URI for the deployed smart contract")
+// .addParam("baseUrl", "The base of the tokenURI endpoint to set")
+// .setAction(async function (taskArguments, hre) {
+//     const contract = await getContract("ASSET0", hre);
+//     const transactionResponse = await contract.setBaseTokenURI(taskArguments.baseUrl, {
+//         gasLimit: 500_000,
+//     });
+//     console.log(`Transaction Hash: ${transactionResponse.hash}`);
+// });
 
 
 task("token-uri", "Fetches the token metadata for the given token ID")
 .addParam("tokenId", "The tokenID to fetch metadata for")
 .setAction(async function (taskArguments, hre) {
-    const contract = await getContract("NFT", hre);
+    const contract = await getContract("ASSET0", hre);
     const response = await contract.tokenURI(taskArguments.tokenId, {
         gasLimit: 500_000,
     });
