e91763827e45405e3e3cdec3c33611e3c92a8ae3
Chris Horn <chris.horn@metacortex.com>
2022-05-04 14:56:22 +0100

    saved

---
+++ contracts/IDZ copy.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+contract IDZ is ERC1155, Ownable, Pausable, ERC1155Supply {
+    uint256 public constant ASSET0 = 1;
+    string public constant name = "ASSET0";
+
+    constructor()
+        ERC1155(
+            "https://bafybeigohx3ya3zy2qchhjwdmamrin2wx76phr5f5x66ryliz5g7654w5i.ipfs.nftstorage.link/metadata/${id}.json"
+        )
+    {
+        _mint(msg.sender, ASSET0, 50000, "");
+    }
+
+    function mint(address account, uint256 id, uint256 amount) public onlyOwner {
+        _mint(account, id, amount, "");
+    }
+
+        /** @dev URI override for OpenSea traits compatibility. */
+
+    function uri(uint256 tokenId) override public pure returns (string memory) {
+        // Tokens minted above the supply cap will not have associated metadata.
+        require(tokenId >= 1 && tokenId <= 50000, "ERC1155Metadata: URI query for nonexistent token");
+        return string(abi.encodePacked("https://bafybeigohx3ya3zy2qchhjwdmamrin2wx76phr5f5x66ryliz5g7654w5i.ipfs.nftstorage.link/metadata/", Strings.toString(tokenId), ".json"));
+    }
+    /*
+     * Override isApprovedForAll to auto-approve OS's proxy contract
+     */
+    function isApprovedForAll(address _owner, address _operator)
+        public
+        view
+        override
+        returns (bool isOperator)
+    {
+        // if OpenSea's ERC1155 Proxy Address is detected, auto-return true
+        if (_operator == address(0x207Fa8Df3a17D96Ca7EA4f2893fcdCb78a304101)) {
+            return true;
+        }
+        // otherwise, use the default ERC1155.isApprovedForAll()
+        return ERC1155.isApprovedForAll(_owner, _operator);
+    }
+}
--- contracts/IDZ.sol
+++ contracts/IDZ.sol
@@ -1,47 +1,108 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.2;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/interfaces/IERC2981.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
-contract IDZ is ERC1155, Ownable {
-    uint256 public constant ASSET0 = 1;
-    string public name = "ASSET0";
+/// @custom:security-contact <security email address>
+contract IDZ is ERC1155, IERC2981, Ownable, Pausable, ContextMixin {
+    using Strings for uint256;
+    string public name;
+    string public symbol;
+    uint256 public total_supply;
+    address private _recipient;
 
-    constructor()
-        ERC1155(
-            "https://bafybeigohx3ya3zy2qchhjwdmamrin2wx76phr5f5x66ryliz5g7654w5i.ipfs.nftstorage.link/metadata/${id}.json"
-        )
-    {
-        _mint(msg.sender, ASSET0, 50000, "");
+    constructor() ERC1155("") {
+        name = "Asset 0";
+        symbol = "IDZ";
+        total_supply = 50000;
+        _recipient = owner();
     }
 
-    function mint(address account, uint256 id, uint256 amount) public onlyOwner {
-        _mint(account, id, amount, "");
+    function pause() public onlyOwner {
+        _pause();
     }
 
-        /** @dev URI override for OpenSea traits compatibility. */
+    function unpause() public onlyOwner {
+        _unpause();
+    }
 
-    function uri(uint256 tokenId) override public pure returns (string memory) {
+    function _beforeTokenTransfer(
+        address operator,
+        address from,
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) internal override whenNotPaused {
+        super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
+    }
+
+    /** @dev URI override for OpenSea traits compatibility. */
+
+    function uri(uint256 tokenId) public view override returns (string memory) {
         // Tokens minted above the supply cap will not have associated metadata.
-        require(tokenId >= 1 && tokenId <= 50000, "ERC1155Metadata: URI query for nonexistent token");
-        return string(abi.encodePacked("https://bafybeigohx3ya3zy2qchhjwdmamrin2wx76phr5f5x66ryliz5g7654w5i.ipfs.nftstorage.link/metadata/", Strings.toString(tokenId), ".json"));
+        require(
+            tokenId >= 1 && tokenId <= total_supply,
+            "ERC1155Metadata: URI query for nonexistent token"
+        );
+        return
+            string(
+                abi.encodePacked(_uriBase, Strings.toString(tokenId), ".json")
+            );
     }
-    /*
-     * Override isApprovedForAll to auto-approve OS's proxy contract
-     */
-    function isApprovedForAll(address _owner, address _operator)
-        public
+
+    /** @dev EIP2981 royalties implementation. */
+
+    // Maintain flexibility to modify royalties recipient (could also add basis points).
+    function _setRoyalties(address newRecipient) internal {
+        require(
+            newRecipient != address(0),
+            "Royalties: new recipient is the zero address"
+        );
+        _recipient = newRecipient;
+    }
+
+    function setRoyalties(address newRecipient) external onlyOwner {
+        _setRoyalties(newRecipient);
+    }
+
+    // EIP2981 standard royalties return.
+    function royaltyInfo(uint256 _tokenId, uint256 _salePrice)
+        external
         view
         override
-        returns (bool isOperator)
+        returns (address receiver, uint256 royaltyAmount)
     {
-        // if OpenSea's ERC1155 Proxy Address is detected, auto-return true
-        if (_operator == address(0x207Fa8Df3a17D96Ca7EA4f2893fcdCb78a304101)) {
-            return true;
-        }
-        // otherwise, use the default ERC1155.isApprovedForAll()
-        return ERC1155.isApprovedForAll(_owner, _operator);
+        return (_recipient, (_salePrice * 1000) / 10000);
+    }
+
+    // EIP2981 standard Interface return. Adds to ERC1155 and ERC165 Interface returns.
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        virtual
+        override(ERC1155, IERC165)
+        returns (bool)
+    {
+        return (interfaceId == type(IERC2981).interfaceId ||
+            super.supportsInterface(interfaceId));
+    }
+
+    /** @dev Meta-transactions override for OpenSea. */
+
+    function _msgSender() internal view override returns (address) {
+        return ContextMixin.msgSender();
+    }
+
+    /** @dev Contract-level metadata for OpenSea. */
+
+    // Update for collection-specific metadata.
+    function contractURI() public pure returns (string memory) {
+        return
+            "ipfs://bafkreigpykz4r3z37nw7bfqh7wvly4ann7woll3eg5256d2i5huc5wrrdq"; // Contract-level metadata for IDZ
     }
 }
