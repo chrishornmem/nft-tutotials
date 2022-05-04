c9a67531f4213161707dfa146e9a8639dd88783c
Chris Horn <chris.horn@metacortex.com>
2022-05-04 12:04:30 +0100

    doesnt deploy

--- contracts/IDZ.sol
+++ contracts/IDZ.sol
@@ -3,13 +3,14 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract IDZ is ERC1155, Ownable {
-    uint256 public constant ASSET0 = 0;
+    uint256 public constant ASSET0 = 1;
 
     constructor()
         ERC1155(
-            "https://bafybeiflvp7bbbsptj7tnya3b2ta5d5l7mm5on3yl74nzxxd4erv7ff5fu.ipfs.nftstorage.link/metadata/${id}.json"
+            "https://bafybeigohx3ya3zy2qchhjwdmamrin2wx76phr5f5x66ryliz5g7654w5i.ipfs.nftstorage.link/metadata/${id}.json"
         )
     {
         _mint(msg.sender, ASSET0, 50000, "");
@@ -17,6 +18,14 @@ contract IDZ is ERC1155, Ownable {
 
     function mint(address account, uint256 id, uint256 amount) public onlyOwner {
         _mint(account, id, amount, "");
+    }
+
+        /** @dev URI override for OpenSea traits compatibility. */
+
+    function uri(uint256 tokenId) override public pure returns (string memory) {
+        // Tokens minted above the supply cap will not have associated metadata.
+        require(tokenId >= 1 && tokenId < 50000, "ERC1155Metadata: URI query for nonexistent token");
+        return string(abi.encodePacked("https://bafybeigohx3ya3zy2qchhjwdmamrin2wx76phr5f5x66ryliz5g7654w5i.ipfs.nftstorage.link/metadata/", Strings.toString(tokenId), ".json"));
     }
     /*
      * Override isApprovedForAll to auto-approve OS's proxy contract
